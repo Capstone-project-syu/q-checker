d17412deee29d9fa16e6230009f7cdc3b5e56c62
suminhand <tnaln47@gmail.com>
2025-05-22 19:33:51 +0900

    [Feat] 05/22 QR 촬영 페이지 제작

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,97 +1,38 @@
-// app/(tabs)/index.tsx
-import Button from "@/components/common/button/Button"; // Button 가져오기
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import React from "react";
-import { Image, Platform, StyleSheet } from "react-native";
+import { Image } from 'expo-image';
 
-export default function IndexScreen() {
+import { Column } from '@/components/layout/Column';
+import { Flex } from '@/components/layout/Flex';
+import { StyleSheet } from 'react-native';
+import ActionButton from '../features/QRMain/sections/ActionButton/ActionButton.section';
+import HistoryList from '../features/QRMain/sections/HistoryList/HistoryList.section';
+
+
+export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D1E8E2", dark: "#1E3A3A" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/logo.svg")} // Q-CHECKER 로고를 예시로
-          style={styles.logoImage}
+    <Column>
+      {/* qr 출결, 생성 버튼 */}
+      <Flex style={styles.actionButtons}>
+        <ActionButton 
+          buttonImage={<Image source={require('@/assets/images/partial-react-logo.png')} />} 
+          buttonText="QR 출결" 
+          linkTo='../features/QRCheck'
         />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome to Q-CHECKER!</ThemedText>
-      </ThemedView>
-
-      {/* Step 1: Button 추가 */}
-      <ThemedView style={styles.stepContainer}>
-        <Button
-          buttonSize="big"
-          fontSize="big"
-          backgroundColor="primary"
-          onPress={() => console.log("버튼 클릭됨!")}
-        >
-          Q-CHECKER 시작하기
-        </Button>
-      </ThemedView>
-
-      {/* Other steps */}
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Start Checking</ThemedText>
-        <ThemedText>
-          Begin your first check by exploring the{" "}
-          <ThemedText type="defaultSemiBold">Checklist</ThemedText> tab.
-        </ThemedText>
-      </ThemedView>
-
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Customize</ThemedText>
-        <ThemedText>
-          Personalize your experience by adjusting your{" "}
-          <ThemedText type="defaultSemiBold">Settings</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Monitor Progress</ThemedText>
-        <ThemedText>
-          Track your performance in{" "}
-          <ThemedText type="defaultSemiBold">Statistics</ThemedText> section.
-        </ThemedText>
-      </ThemedView>
-
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Shortcut for Dev Tools</ThemedText>
-        <ThemedText>
-          Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        <ActionButton 
+          buttonImage={<Image source={require('@/assets/images/partial-react-logo.png')} />} 
+          buttonText="QR 생성" 
+          linkTo='../features/QRGenerate'
+        />
+      </Flex>
+      {/* qr 생성 내역 */}
+      <HistoryList />
+    </Column>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-    marginBottom: 16,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 16,
-  },
-  logoImage: {
-    height: 60,
-    width: 300,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
+  actionButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 30,
+  }
+})
